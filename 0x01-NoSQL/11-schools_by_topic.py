2793911f255055b227b8e4390ea2c667fcfa1cb5
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-18 19:25:24 +0300

    finsh task

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Returns the list of school having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school
+    """
+    cursor = mongo_collection.find({"topics": topic})
+    return cursor
