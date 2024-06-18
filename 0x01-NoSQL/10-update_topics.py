977e1ad36fbecdf75f89044c7cd865b4411e209a
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-18 19:22:43 +0300

    finsh task

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Changes all topics of a school document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school
+    """
+    cursor = mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+    return cursor
