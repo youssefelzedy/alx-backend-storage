0adf18945bb0084fb3f35afa83b2f6c0d32f297f
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-18 19:07:43 +0300

    finsh task

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,8 +3,11 @@
 Lists all documents in a collection
 """
 
+
 def list_all(mongo_collection):
-    # Use find() to retrieve all documents in the collection
+    """
+    Use find() to retrieve all documents in the collection
+    """
     cursor = mongo_collection.find()
     
     return cursor
